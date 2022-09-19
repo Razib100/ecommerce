113d1ba136d9367d7018e6a5fd4f5cc0ac4e5349
razibVeefin <razib@veefin.com>
2022-09-19 15:59:23 +0600

    banner add

--- app/Http/Controllers/frontend/IndexController.php
+++ app/Http/Controllers/frontend/IndexController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Banner;
 
 class IndexController extends Controller
 {
     public function home(){
-        return view('frontend.dashboard');
+        $banners = Banner::where(['status' => 'active', 'condition' => 'banner'])->limit(3)->orderby('id', 'DESC')->get();
+        return view('frontend.dashboard',compact('banners'));
     }
 }
